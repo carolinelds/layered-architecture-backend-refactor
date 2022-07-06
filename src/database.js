bac49a24bf40645221ae6c8395fe82b793395660
Caroline LS <carolinelds@gmail.com>
2022-07-06 16:23:50 -0300

    fix: new attempt to solve issue with database connection

--- src/database.js
+++ src/database.js
@@ -1,9 +1,36 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
-const { Pool } = pg;
+let chachedDB = null;
+let connectionParams = {
+};
 
-const connection = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
 
-export default connection;
+export default async function connectDB() {
+  if (chachedDB) {
+    return chachedDB;
+  }
+
+  if (process.env.DATABASE_URL) {
+    connectionParams = {
+      connectionString: process.env.DATABASE_URL,
+    };
+  }
+
+  if (process.env.MODE === "PROD") {
+    connectionParams.ssl = {
+      rejectUnauthorized: false,
+    };
+  }
+
+  const { Pool } = pg;
+
+  const connection = new Pool(connectionParams);
+
+  await connection.connect();
+
+  chachedDB = connection;
+
+  return connection;
+}
--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,21 +1,22 @@
-import connection from "./../database.js";
+//import connection from "./../database.js";
+import connectDB from "./../database.js";
 import errorResponse from "./../responses/errorResponses.js";
 
 export async function signUpService(name, email, password) {
+
+    console.log("chega aqui");
+    const connection = await connectDB();
+    console.log("não chega aqui");
+
 
     if (!name || !email || !password) {
         return errorResponse.emptyInput("User data fields");
     }
 
-    console.log("chega aqui");
-    console.log(connection);
-
     const existingUsers = await connection.query(
         `SELECT * FROM "users" WHERE "email"=$1`,
         [email]
     );
-
-    console.log("não chega aqui");
 
     if (existingUsers.rowCount > 0) {
         return errorResponse.databaseConflict("Email");
